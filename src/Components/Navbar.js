f852ba012c4c3e056ad4c19fbe3ebd1237aa0812
Karan <karanchandekar431@gmail.com>
2023-01-11 19:51:30 +0530

    created and styled a display modal for about

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -31,6 +31,49 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
+
+      {/* Modal div */}
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header ">
+              <h5 className="modal-title " id="exampleModalLabel">
+                Emoji Search App
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              In this emoji search app, you can search any emoji which is
+              available. There is an another feature in this app where you can
+              simply copy an emoji of your choice to your clipboard.
+              <h4 className="pt-3">Developed with ❤️ by Karan Chandekar.</h4>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
