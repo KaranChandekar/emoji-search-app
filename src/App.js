00f34cc55abbb9ff618469c1b121a2e497433997
Karan <karanchandekar431@gmail.com>
2023-01-11 21:38:22 +0530

    created heading and search input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import EmojiList from "./emojiList.json";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-    </React.Fragment>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      emojiList: EmojiList,
+      inputValue: "",
+      title: "",
+    };
+
+    this.searchEmoji = this.searchEmoji.bind(this);
+  }
+
+  searchEmoji = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div className="container ">
+          <h1 className="heading  pb-5">🦄 Emoji Search App</h1>
+          <div className="col-md-6 mb-4 mx-auto">
+            <div className="input-group md-form form-sm form-2 pl-0">
+              <div className="input-group-append">
+                <span
+                  className="input-group-text amber lighten-3"
+                  id="basic-text1"
+                >
+                  <i className="fa fa-search text-grey" aria-hidden="true"></i>
+                </span>
+              </div>
+              <input
+                className="form-control my-0 py-1 "
+                onChange={(e) => {
+                  this.searchEmoji(e);
+                }}
+                type="text"
+                placeholder="Search for an emoji"
+                aria-label="Search"
+              ></input>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 export default App;
