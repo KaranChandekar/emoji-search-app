c9cd95969a14c2552b7fbacc7a659078467be5d1
Karan Chandekar <karanchandekar431@gmail.com>
2023-03-01 15:43:13 +0530

    update class component into functional component

--- src/App.js
+++ src/App.js
@@ -1,99 +1,85 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import EmojiList from './emojiList.json';
 import './App.css';
 import Navbar from './Components/Navbar';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [emojiList, setEmojiList] = useState(EmojiList);
+  const [inputValue, setInputValue] = useState('');
+  const [title, setTitle] = useState('');
 
-    this.state = {
-      emojiList: EmojiList,
-      inputValue: '',
-      title: '',
-    };
-
-    this.searchEmoji = this.searchEmoji.bind(this);
-    this.copyToClipboard = this.copyToClipboard.bind(this);
-  }
-
-  searchEmoji = (e) => {
-    this.setState({ inputValue: e.target.value });
+  const searchEmoji = (e) => {
+    setInputValue(e.target.value);
   };
 
-  copyToClipboard = (title, symbol) => {
+  const copyToClipboard = (title, symbol) => {
     navigator.clipboard.writeText(symbol);
-    this.setState({ title: title });
+    setTitle(title);
     setTimeout(() => {
-      this.setState({ title: '' });
+      setTitle('');
     }, 4000);
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <div className="container ">
-          <h1 className="heading  pb-5">🦄 Emoji Search App</h1>
+  return (
+    <React.Fragment>
+      <Navbar />
+      <div className="container ">
+        <h1 className="heading  pb-5">🦄 Emoji Search App</h1>
 
-          {/* Search Bar */}
-          <div className="col-md-6 mb-4 mx-auto">
-            <div className="input-group md-form form-sm form-2 pl-0">
-              <div className="input-group-append">
-                <span
-                  className="input-group-text amber lighten-3"
-                  id="basic-text1"
-                >
-                  <i className="fa fa-search text-grey" aria-hidden="true"></i>
-                </span>
-              </div>
-              <input
-                className="form-control my-0 py-1 "
-                onChange={(e) => {
-                  this.searchEmoji(e);
-                }}
-                type="text"
-                placeholder="Search for an emoji"
-                aria-label="Search"
-              ></input>
+        {/* Search Bar */}
+        <div className="col-md-6 mb-4 mx-auto">
+          <div className="input-group md-form form-sm form-2 pl-0">
+            <div className="input-group-append">
+              <span
+                className="input-group-text amber lighten-3"
+                id="basic-text1"
+              >
+                <i className="fa fa-search text-grey" aria-hidden="true"></i>
+              </span>
             </div>
-          </div>
-
-          {/* Emoji Container */}
-          <div className="row mx-auto ">
-            {this.state.emojiList
-              .filter((value) => {
-                return value.keywords
-                  .toLowerCase()
-                  .includes(this.state.inputValue.toLowerCase());
-              })
-              .map((el) => {
-                return (
-                  <div
-                    className="card emojiCard  m-3   card-columns mx-auto d-flex justify-content-center "
-                    data-toggle="tooltip"
-                    data-placement="bottom"
-                    title="Click here to Copy"
-                    onClick={() => this.copyToClipboard(el.title, el.symbol)}
-                  >
-                    <div className="emoji">
-                      <span
-                        className="emoji"
-                        ref={(copyEmoji) => (this.copyEmoji = copyEmoji)}
-                      >
-                        {el.symbol}
-                      </span>
-                      {this.state.title === el.title && (
-                        <p className="fade-in ">Copied!</p>
-                      )}
-                    </div>
-                  </div>
-                );
-              })}
+            <input
+              className="form-control my-0 py-1 "
+              onChange={searchEmoji}
+              type="text"
+              placeholder="Search for an emoji"
+              aria-label="Search"
+            ></input>
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
+
+        {/* Emoji Container */}
+        <div className="row mx-auto ">
+          {emojiList
+            .filter((value) => {
+              return value.keywords
+                .toLowerCase()
+                .includes(inputValue.toLowerCase());
+            })
+            .map((el) => {
+              return (
+                <div
+                  className="card emojiCard  m-3   card-columns mx-auto d-flex justify-content-center "
+                  data-toggle="tooltip"
+                  data-placement="bottom"
+                  title="Click here to Copy"
+                  onClick={() => copyToClipboard(el.title, el.symbol)}
+                >
+                  <div className="emoji">
+                    <span
+                      className="emoji"
+                      ref={(copyEmoji) =>
+                        copyEmoji && (copyEmoji.innerHTML = el.symbol)
+                      }
+                    ></span>
+                    {title === el.title && <p className="fade-in ">Copied!</p>}
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
+
 export default App;
